38cb3c5e1122990d77ab465fe17b6aaf7790120a
Kelly Marie Nighbert <Kelly@Kellys-MacBook-Pro.local>
2021-02-03 19:30:48 -0500

    message

--- db.js
+++ db.js
@@ -3,7 +3,13 @@ const Sequelize = require('sequelize');
 
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
+  dialect: 'postgres',
+  dialectOptions: process.env.DATABASE_URL.includes(`postgres`) ? {}: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION 
+    },
+  },
 });
 
 sequelize .authenticate().then(
