c8af858430f2e0f7c8f41712bd2d264abc027f22
Kelly Marie Nighbert <Kelly@Kellys-MacBook-Pro.local>
2021-02-03 21:22:26 -0500

    project

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
     case '127.0.0.1':
         APIURL = 'http://localhost:3000'
         break
-    case 'kelnighbo-frontapp.herokuapp.com':
+    case 'https://kelnighbo-frontapp.herokuapp.com':
         APIURL = 'https:kelnighbo-app.herokuapp.com'
 
 }
