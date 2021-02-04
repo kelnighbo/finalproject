1550ef0c2e2877bbc778d31aabaab576700b8d28
Kelly Marie Nighbert <Kelly@Kellys-MacBook-Pro.local>
2021-02-03 21:34:51 -0500

    final

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,12 +1,12 @@
 let APIURL = ''
 
 switch (window.location.hostname) {
+    case 'https://kelnighbo-frontapp.herokuapp.com':
+        APIURL = 'https://kelnighbo-app.herokuapp.com';
+    break;
     case 'localhost':
     case '127.0.0.1':
         APIURL = 'http://localhost:3000'
-        break
-    case 'https://kelnighbo-frontapp.herokuapp.com':
-        APIURL = 'https://kelnighbo-app.herokuapp.com'
 
 }
 
